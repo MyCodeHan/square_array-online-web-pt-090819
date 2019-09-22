95714af4be11bf50ce49e99d12bd0284e8a9ada2
Hannah Harrington <hannahharrington@outlook.com>
2019-09-22 01:40:54 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,6 @@ def square_array(numbers)
   # your code here	  
   new_numbers = [1,2,3]
 end 	  numbers.each do |num|
-    new_numbers << (num ** 2)
+    new_numbers << 
   end
   return new_numbers
