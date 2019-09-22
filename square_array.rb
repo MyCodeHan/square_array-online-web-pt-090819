a443e0a8e617851356018903c0f52b69ab3179d1
Hannah Harrington <hannahharrington@outlook.com>
2019-09-22 01:50:19 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,16 +1,12 @@
-ef square_array(array)
-  new_array = []
-  array.each{|a| new_array.push(a*a)}
-  return new_array
+def square_array(numbers)
+  # your code here	  new_numbers = []
+end 	  numbers.each do |num|
+    new_numbers << (num ** 2)
+  end
+  return new_numbers
 end
 
-arr = [1, 2, 3, 4, 5]
-square_array(arr)
-# //arr.each { |a| print a -= 10, " " }
-# # prints: -9 -8 -7 -6 -5
-#=> [1, 2, 3, 4, 5]
-
-end
-
-
-# basket.each do |apple|
+# def square_array(numbers)
+#   new_numbers = numbers.collect{|num| num ** 2}
+#   return new_numbers
+# end
